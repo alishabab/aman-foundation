18a3c3307184a2988b4bf5533e2c50e3ddb2d664
Shabab Ali <shababsaifi@gmail.com>
2022-04-02 11:55:58 +0530

    updated footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,3 +1,48 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faFacebookSquare,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 export const Footer = () => {
-  return <footer>I am a footer</footer>;
+  return (
+    <footer className="border-t-[0.0125rem] border-gray-300 bg-gray-100 py-8 px-4">
+      <div className="flex flex-col items-center">
+        <div className="flex items-center space-x-16">
+          <FontAwesomeIcon
+            icon={faFacebookSquare}
+            size="2x"
+            className="text-primary-700"
+          />
+          <FontAwesomeIcon
+            icon={faTwitter}
+            size="2x"
+            className="text-primary-700"
+          />
+          <FontAwesomeIcon
+            icon={faInstagram}
+            size="2x"
+            className="text-primary-700"
+          />
+        </div>
+        {/* <p className="text-gray-500 text-sm mt-4">
+        Built in this life, for the next.
+      </p> */}
+        <p className="text-gray-500 text-sm mt-8">
+          Made with{" "}
+          <FontAwesomeIcon icon={faHeart} className="text-primary-700" /> by
+          Shabab
+        </p>
+        <p className="text-xs text-gray-400"></p>
+      </div>
+      <div className="mt-8 text-center">
+        <p className="text-xs text-gray-500">© 2022 AmanFoundation</p>
+        <p className="text-xs text-gray-500 mt-2">
+          Having issues? Report it here <FontAwesomeIcon icon={faGithub} />
+        </p>
+      </div>
+    </footer>
+  );
 };
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 const Home: NextPage = ({ children }) => {
-  return <div className="border border-indigo-600">Index page</div>;
+  return <div className="">Index page</div>;
 };
 
 export default Home;
