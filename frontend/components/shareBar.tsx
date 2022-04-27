fec322e82376fbf295e4894c2ae535d9c0db4fd0
Shabab Ali <shababsaifi@gmail.com>
2022-04-27 15:01:48 +0530

    made share buttons functional

--- frontend/components/shareBar.tsx
+++ frontend/components/shareBar.tsx
@@ -9,14 +9,61 @@ import { faShare, faCopy } from "@fortawesome/free-solid-svg-icons";
 import { NextPage } from "next";
 import { CSSProperties, useState } from "react";
 import { Button } from "./button";
+import { useAlert } from "context/AlertContext";
 
 interface Props {
   className?: string;
   style?: CSSProperties;
 }
+
+type SocialMediaType =
+  | "facebook"
+  | "twitter"
+  | "instagram"
+  | "whatsapp"
+  | "other";
+
 export const ShareBar: NextPage<Props> = ({ className, style }) => {
   const [isShown, setIsShown] = useState(true);
-  const url = window?.location.href;
+  const { setAlert } = useAlert();
+  const getUrl = () => {
+    const url = window.location.href;
+    return url;
+  };
+
+  const getText = () =>
+    `Support this campaign by Aman Foundation. Visit ${getUrl()}`;
+
+  const onClick = (type: SocialMediaType) => {
+    switch (type) {
+      case "facebook":
+        window.open(
+          `https://www.facebook.com/sharer/sharer.php?u=${getUrl()}`,
+          "_blank"
+        );
+        return;
+      case "whatsapp":
+        window.open(`whatsapp://send?text=${getText()}`, "_blank");
+        return;
+      case "twitter":
+        window.open(
+          `http://twitter.com/intent/tweet/?url=${getUrl()}`,
+          "_blank"
+        );
+        return;
+      default:
+        onCopy();
+    }
+  };
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(getText());
+    setAlert({
+      type: "success",
+      message: "Text Copied.",
+    });
+  };
+
   return (
     <div
       className={`relative h-16 bg-white border-t border-gray-100 flex items-center justify-between py-2 px-4 ${
@@ -37,23 +84,19 @@ export const ShareBar: NextPage<Props> = ({ className, style }) => {
           <span className="text-sm font-bold">SHARE</span>
         </div>
         <div className="flex items-center space-x-3 text-secondary-600">
-          <a
-            href={`whatsapp://send?text=Support this campaign by Aman Foundation. Visit ${url}`}
-            data-action="share/whatsapp/share"
-            target="_blank"
-            rel="noreferrer">
+          <button onClick={() => onClick("whatsapp")}>
             <FontAwesomeIcon icon={faWhatsapp} size="2x" />
-          </a>
-          <button>
+          </button>
+          <button onClick={() => onClick("facebook")}>
             <FontAwesomeIcon icon={faFacebookSquare} size="2x" />
           </button>
-          <button>
+          {/* <button>
             <FontAwesomeIcon icon={faInstagram} size="2x" />
-          </button>
-          <button>
+          </button> */}
+          <button onClick={() => onClick("twitter")}>
             <FontAwesomeIcon icon={faTwitter} size="2x" />
           </button>
-          <button>
+          <button onClick={() => onClick("other")}>
             <FontAwesomeIcon icon={faCopy} size="2x" />
           </button>
         </div>
