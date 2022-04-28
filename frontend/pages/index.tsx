c5490e325203f878a9cd470cf9903dfe88f793c0
Shabab Ali <shababsaifi@gmail.com>
2022-04-28 20:32:24 +0530

    rolled back to getServerSideProps

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -10,7 +10,7 @@ import {
   useGetQODQuery,
 } from "services/queries";
 import { CacheKeys } from "services/cacheKeys";
-import { GetServerSideProps, GetStaticProps } from "next";
+import { GetServerSideProps } from "next";
 import Link from "next/link";
 import {
   faBowlRice,
@@ -257,7 +257,7 @@ export default function Hero() {
   );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery(CacheKeys.Campaigns, getCampaigns);
