cb27b6d0fb7de0a3294408c6818a90b3124114af
Shabab Ali <shababsaifi@gmail.com>
2022-09-29 16:42:17 +0530

    added nprogress bar

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
+import Router from "next/router";
+import nProgress from "nprogress";
+import "nprogress/nprogress.css";
+import "../styles/nprogress.css";
 import "../styles/globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
 import type { AppProps } from "next/app";
-import NextNProgress from "nextjs-progressbar";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { SessionProvider } from "next-auth/react";
 
 import { Layout } from "components";
 import { AlertProvider } from "context/AlertContext";
+
+nProgress.configure({ showSpinner: false });
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
   return (
@@ -19,7 +28,6 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
         <Hydrate state={pageProps.dehydratedState}>
           <AlertProvider>
             <Layout>
-              <NextNProgress color="#e27413" />
               <Component {...pageProps} />
             </Layout>
           </AlertProvider>
